0a77ef2d336ec62ddc5d3f832dab6173927a49ba
branweb <branweb1@gmail.com>
2018-09-03 23:03:47 -0700

    basic styling

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,6 @@
 class EntriesController < ApplicationController
   def index
-    @entries = Entry.paginate(page: params[:page])
+    @entries = Entry.order(:topic).paginate(page: params[:page], per_page: 45)
   end
 
   def show
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+
+  self.per_page = 45
+
 end
